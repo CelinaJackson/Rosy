8df4896d8a87791ca1f77de2442edabb84bb6886
CelinaJackson <celinanjackson@gmail.com>
2021-02-03 15:55:52 -0500

    created User and Wine models and tables

---
+++ db/migrate/20210203205439_create_wines.rb
@@ -0,0 +1,11 @@
+class CreateWines < ActiveRecord::Migration[6.0]
+  def change
+    create_table :wines do |t|
+      t.string :name
+      t.string :region
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
