e6f7c4ede50d19aee6e00b11dd4c0e3569969d39
CelinaJackson <celinanjackson@gmail.com>
2021-02-04 15:33:53 -0500

    creating files

---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,27 @@
+class RatingsController < ApplicationController 
+    def index 
+    end 
+
+    def show 
+    end 
+
+    def new 
+    end 
+
+    def create 
+    end 
+
+    def edit 
+    end 
+
+    def update
+    end 
+
+    def destroy 
+    end 
+
+    private 
+
+    def rating_params 
+    end 
+end
