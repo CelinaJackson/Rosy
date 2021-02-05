0f7aac1e66a72e4de24b981f4eb3996c0c41449f
CelinaJackson <celinanjackson@gmail.com>
2021-02-05 14:16:01 -0500

    started building out methods in controllers

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,24 +1,54 @@
 class WinesController < ApplicationController 
     def index 
+        @wines = Wine.all
     end 
 
     def show 
+        @wine = Wine.find(params[:id])
     end 
 
     def new 
+        @wine = Wine.new
     end 
 
     def create 
+        @wine = Wine.new(wine_params)
+        if @wine.save 
+            redirect_to wine_path(@wine)
+        else 
+            redirect_to new_wine_path 
+        end 
     end 
 
     def edit 
+        @wine = Wine.find(params[:id])
     end 
 
     def update
+        @wine = Wine.find(params[:id])
+        if @wine.user == current_user 
+            @wine.update(wine_params)
+        else 
+            flash[:alert] = "You can't edit a wine someone else added."
+            redirect_to wine_path(@wine)
+        end 
     end 
+
+    def destroy 
+        @wine = Wine.find(params[:id])
+        if @wine.user == current_user 
+            @wine.destroy 
+            flash[:alert] = "Wine was deleted."
+            redirect_to wines_path 
+        else 
+            flash[:alert] = "You can't delete a wine someone else added."
+            redirect_to wine_path(@wine)
+        end
+    end
 
     private 
 
     def wine_params 
+        params.require(:wine).permit(:name, :region, :type)
     end 
 end
