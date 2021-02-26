643a707510260ca0cbad29a9f8a851a8f3d351b4
CelinaJackson <celinanjackson@gmail.com>
2021-02-26 15:30:05 -0500

    changing nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root 'sessions#index'
-  resources :sessions, :users, :ratings, :wines 
+  resources :sessions, :ratings
   resources :wines do 
-    resources :ratings 
+    resources :ratings, only: [:index, :show, :new, :create]
   end 
   resources :users do 
-    resources :ratings  
+    resources :ratings, only: [:index, :show] 
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
